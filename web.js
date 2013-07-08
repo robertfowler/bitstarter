33cf8d73995c5b28478b0288d9a1c6cad1e6c080
Ubuntu <ubuntu@ip-172-31-29-240.us-west-2.compute.internal>
2013-07-08 05:57:38 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-//var buf = new Buffer(100);
  
 var app = express.createServer(express.logger());
 var inputFile = './index.html';
 
 app.get('/', function(request, response) {
-    //buf = fs.readFileSync(inputFile, 'utf-8');
-    //response.send(buf);
-response.send(fs.readFileSync(inputFile, 'utf-8'));
+      response.send(fs.readFileSync(inputFile, 'utf-8'));
 });
 
 
